4b85191ba092f6c533ad2621d895967961d23a5c
Amine RTU <50716490+AMAMinou@users.noreply.github.com>
2020-12-13 02:27:14 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,69 @@
+export default App;
+import React from 'react';
+
+
+class App extends React.Component {
+
+    /*
+     constructor
+     
+     */
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+
+    }
+
+    /**
+     * componentDidMount
+     *
+     * Fetch json array of objects from given url and update state.
+     */
+    componentDidMount() {
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true, 
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    }
+
+    /**
+     * render
+
+     */
+    render() {
+
+        const { isLoaded, items } = this.state;
+
+        if (!isLoaded)
+            return <div>Loading...</div>;
+
+        return (
+            <div className="App">
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            Name: {item.name} |  Email: {item.email} |  Username: {item.username}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+
+    }
+
+}
+
+export default App;
